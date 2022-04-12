42c45f15bd5ce01407e8c5979d1d3dfc815af00f
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-12 17:35:27 +0600

    search result

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post, Catagory
 # Create your views here.
+from django.contrib import messages
 
 
 def home(request):
@@ -13,3 +14,12 @@ def home(request):
 def blogDetails(request, slug):
     blog = Post.objects.get(slug=slug)
     return render(request, 'blog/blogdetails.html', context={'post': blog})
+
+
+def search(request):
+    if request.method == 'POST':
+        search = request.POST['search_file']
+        search_file = Post.objects.filter(title__icontains=search)
+
+    context = {'search_file': search_file}
+    return render(request, 'blog/search.html', context)
