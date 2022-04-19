f8614d02b251816f6179abf11b3183e785206dda
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-19 07:01:35 +0600

    Blog Post Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,9 @@
 from dataclasses import fields
 from rest_framework import serializers
 from blog.models import Post
+
+
+class Postserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
