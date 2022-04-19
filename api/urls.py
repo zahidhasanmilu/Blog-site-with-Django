8f3672ae44c0f1a97408a4d0669c711c62fcb995
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-04-19 06:58:42 +0600

    CRUD urls create , read, update, delete

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('', views.all_blogs, name='allblogs'),
     path('<int:id>/', views.single_blog, name='single_blog'),
     path('update-api/<int:id>/', views.update_blog, name='update_blog'),
-    path('create-api/', views.create_blog, name='create_blog'),
+    path('create-api/', views.create_blog, name='create_ blog'),
     path('delete_blog/<int:id>', views.delete_blog, name='delete_blog'),
 ]
